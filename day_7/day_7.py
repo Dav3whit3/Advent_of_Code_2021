1fb3872338f2f8f7faef5a0a938a14a72ee4f837
David Blanco <davidblanco@Davids-MacBook-Pro.local>
2021-12-13 01:16:58 +0100

    day_7

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -1,33 +1,24 @@
 import sys
-from typing import Generator
 sys.path.append('/Users/davidblanco/Documents/personal/Advent_of_Code_2021')
-from utils.tools import input_parser, post_answer, current_stars
-from collections import deque
+from utils.tools import input_parser, current_stars
 from functools import reduce
+import math
 
 
 
-def solution(arr: list, part=1) -> int:
-    max_cost = summ_of_diffs(arr, max(arr), part)
-    return is_lower(arr, part, last_sum=max_cost)
-
-
-def is_lower(array: list, part=1, current_value=0, last_sum=0) -> int:
+def solution(array, part=1, current_value=0, last_sum=math.inf):
     cost = summ_of_diffs(array, current_value, part)
-    return is_lower(array, part, current_value+1, last_sum=cost) if cost < last_sum else last_sum
+    return solution(array, part, current_value+1, last_sum=cost) if cost < last_sum else last_sum
 
 
-def summ_of_diffs(arr: list, val: int, part=1) -> int:
+def summ_of_diffs(array, val, part=1):
     if part==2:
-        return reduce(lambda a,b: a+b, ( factorial_summ(abs(i - val)) for i in arr) )    
-    return reduce(lambda a,b: a+b, (abs(i - val) for i in arr))
+        return reduce(lambda a,b: a+b, ( factorial_summ(abs(i - val)) for i in array) )    
+    return reduce(lambda a,b: a+b, (abs(i - val) for i in array))
 
 
-def factorial_summ(n: int) -> int:
+def factorial_summ(n):
     return reduce(lambda a,b: a+b, range(n+1))
-
-
-
 
 
 
@@ -40,3 +31,4 @@ if __name__ == '__main__':
     print(f"Example Part 2: {solution(ex, part=2)}")
     print(f"Solution Part 1: {solution(input_7)}")
     print(f"Solution Part 2: {solution(input_7, part=2)}")
+    print(current_stars())
